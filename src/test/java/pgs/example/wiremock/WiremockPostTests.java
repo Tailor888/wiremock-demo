58aa359c667936312f8ea5f2ce62984da1a7fc0e
pkrawiec <1orzysz12>
2017-02-06 17:08:40 +0100

    added another post case

--- src/test/java/pgs/example/wiremock/WiremockPostTests.java
+++ src/test/java/pgs/example/wiremock/WiremockPostTests.java
@@ -11,10 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import pgs.example.wiremock.model.SampleRequest;
+import pgs.example.wiremock.model.SampleResponse;
+import wiremock.com.fasterxml.jackson.core.JsonProcessingException;
+import wiremock.com.fasterxml.jackson.databind.ObjectMapper;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by pkrawiec on 1/30/2017.
@@ -22,6 +26,8 @@ import static io.restassured.RestAssured.given;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WiremockPostTests {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(8089);
@@ -53,5 +59,26 @@ public class WiremockPostTests {
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON.toString()).withBody(sampleResponse)));
 
         given().body(sampleRequest, ObjectMapperType.GSON).when().post(url).then().assertThat().statusCode(200).and().assertThat().body(Matchers.equalTo(sampleResponse));
+    }
+
+    @Test
+    public void postResponse_object_success() throws JsonProcessingException {
+        String url = "/pgs/resource/1";
+        String city = "Somewhere";
+
+        SampleResponse sampleResponse = new SampleResponse();
+        sampleResponse.setId(1);
+        sampleResponse.setName("Someone");
+        sampleResponse.setCity(city);
+
+        SampleRequest sampleRequest = new SampleRequest();
+        sampleRequest.setId(1);
+
+        givenThat(post(urlEqualTo(url)).withRequestBody(containing("\"id\":1"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON.toString()).withBody(objectMapper.writeValueAsString(sampleResponse))));
+
+        given().body(sampleRequest, ObjectMapperType.GSON).when().post(url).then().assertThat().statusCode(200).and().assertThat().body("city", is(city));
     }
 }
